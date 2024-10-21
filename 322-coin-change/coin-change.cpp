59a8aa48c3b01e5f9ecef36b0c1fb3ff9723821b
teotiaaditya <112480618+teotiaaditya@users.noreply.github.com>
2024-10-21 17:05:55 +0530

    Time: 23 ms (76.93%) | Memory: 16.4 MB (72.49%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -36,6 +36,21 @@ public:
         dp[amount]=mini;
         return dp[amount];
     }
+
+    //tabulation (bottom up approach)
+    int tabulation(vector<int>& coins, int amount){
+        vector<int> dp(amount+1, INT_MAX);
+        dp[0]=0;
+
+        for(int i=0;i<=amount;i++){
+            // calculate foe every i from 0 to amount
+            for(int j=0;j<coins.size();j++){
+                if(i-coins[j] >= 0 && dp[i-coins[j]] != INT_MAX)
+                    dp[i]=min(dp[i], 1 + dp[i-coins[j]]);
+            }
+        }
+        return dp[amount];
+    }
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
         // // recurtion
@@ -45,8 +60,16 @@ public:
         // return ans;
 
 
-        vector<int> dp(amount+1, -1);
-        int ans= memoisation(coins, amount, dp);
+        // // memoisation
+        // vector<int> dp(amount+1, -1);
+        // int ans= memoisation(coins, amount, dp);
+        // if(ans==INT_MAX)
+        //     return -1;
+        // return ans;
+
+
+        // tabulation
+        int ans= tabulation(coins, amount);
         if(ans==INT_MAX)
             return -1;
         return ans;

d6eb3390a11ba26299b3d3e24e03eeca4e6809d2
teotiaaditya <112480618+teotiaaditya@users.noreply.github.com>
2024-10-21 21:21:36 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (72.40%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int solve(vector<int> &nums){
+        int n=nums.size();
+        
+        int prev2=0;
+        int prev1=nums[0];
+
+        for(int i=1;i<n;i++){
+            int include= prev2 + nums[i];
+            int exclude= prev1 +0;
+            int ans = max(include, exclude);
+
+            prev2=prev1;
+            prev1=ans;
+        }
+        return prev1;
+    }
+    int rob(vector<int>& nums) {
+        vector<int> vec1,vec2;
+        int n=nums.size();
+        if(n==1)
+            return nums[0];
+
+        for(int i=0;i<n; i++){
+            if(i!= n-1)
+                vec1.push_back(nums[i]);
+            if(i != 0)
+                vec2.push_back(nums[i]);
+        }
+        return max(solve(vec1), solve(vec2));
+    }
+};

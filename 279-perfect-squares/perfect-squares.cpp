ae2fae8f42eabd139c9c611a89c16be6d58bbf47
teotiaaditya <112480618+teotiaaditya@users.noreply.github.com>
2025-01-02 21:34:42 +0530

    Time: 70 ms (48.91%) | Memory: 13.8 MB (29.30%) - LeetSync

---
+++ 279-perfect-squares/perfect-squares.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int solvemem(int n, vector<int> &dp){
+        if(n==0){
+            return 0;
+        }
+        if(dp[n] !=-1)
+            return dp[n];
+        int ans=n;
+        for(int i=1;i*i<=n;i++){
+            ans=min( ans, 1+ solvemem(n-(i*i),dp) );
+        }
+        dp[n]=ans;
+        return dp[n];
+    }
+    int solvetab(int n){
+        vector<int> dp(n+1, INT_MAX);
+        dp[0]=0;
+        for(int j=1;j<=n;j++){
+            for(int i=1;i*i<=n;i++){
+                dp[j]=min( dp[j], 1+ dp[j-(i*i)] );
+            }
+        }
+        return dp[n];
+    }
+    int numSquares(int n) {
+        	vector<int> dp(n+1,-1);
+        	return solvemem(n,dp);
+
+            // return solvetab(n);
+        }
+};

1cd0daee2e909f0f79262d536a26a8c3f6ff1473
teotiaaditya <112480618+teotiaaditya@users.noreply.github.com>
2025-01-16 15:15:41 +0530

    Time: 11 ms (49.08%) | Memory: 25.3 MB (6.29%) - LeetSync

--- 1503-reducing-dishes/reducing-dishes.cpp
+++ 1503-reducing-dishes/reducing-dishes.cpp
@@ -10,6 +10,8 @@ public:
         return max(include, exclude);
 
     }
+
+
     int solveMem(vector<int>& satisfaction, int index, int time,vector<vector<int>> &dp){
         if(index == satisfaction.size()){
             return 0;
@@ -23,9 +25,47 @@ public:
         return dp[index][time];
 
     }
+
+
+    int solveTab(vector<int>& satisfaction){
+        int n=(satisfaction.size());
+        vector<vector<int>> dp(n+1, vector<int> (n+1, 0));
+
+        for(int index=n-1; index>=0; index--){
+            for(int time=index; time>=0; time--){
+                int include= satisfaction[index] * (time+1) + dp[index+1][time+1];
+                int exclude= 0 + dp[index+1][time];
+                dp[index][time]=max(include, exclude);
+            }
+        }
+        return dp[0][0];
+    }
+
+
+    int solveSO(vector<int>& satisfaction){
+        int n=(satisfaction.size());
+        vector<int> curr(n+1, 0);
+        vector<int> next(n+1, 0);
+        
+        for(int index=n-1; index>=0; index--){
+            for(int time=index; time>=0; time--){
+                int include= satisfaction[index] * (time+1) + next[time+1];
+                int exclude= 0 + next[time];
+                curr[time]=max(include, exclude);
+            }
+            next=curr;
+        }
+        return curr[0];
+    }
+
+
     int maxSatisfaction(vector<int>& satisfaction) {
         sort(satisfaction.begin(), satisfaction.end());
         vector<vector<int>> dp(satisfaction.size()+1, vector<int> (satisfaction.size()+1, -1));
-        return solveMem(satisfaction, 0, 1, dp);
+        // return solveMem(satisfaction, 0, 1, dp);
+
+        // return solveTab(satisfaction);
+
+        return solveSO(satisfaction);
     }
 };
